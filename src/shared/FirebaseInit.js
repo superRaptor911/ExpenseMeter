79da8fe663c239b04f06efa2779add10be809e69
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-01 11:58:48 +0530

    Fixed firebase Test fail

--- src/shared/FirebaseInit.js
+++ src/shared/FirebaseInit.js
@@ -7,13 +7,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const FirebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: process.env.REACT_APP_apiKey || 'gg',
+  authDomain: process.env.REACT_APP_authDomain || 'gg',
+  projectId: process.env.REACT_APP_projectId || 'gg',
+  storageBucket: process.env.REACT_APP_storageBucket || 'gg',
+  messagingSenderId: process.env.REACT_APP_messagingSenderId || 'gg',
+  appId: process.env.REACT_APP_appId || 'gg',
+  measurementId: process.env.REACT_APP_measurementId || 'gg',
 };
 
 firebase.initializeApp(FirebaseConfig);
