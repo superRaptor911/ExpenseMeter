85b26e3b249492596d417517d7d88309b9a836a3
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-01 13:15:11 +0530

    Email signin

--- src/shared/Authentication.js
+++ src/shared/Authentication.js
@@ -23,25 +23,26 @@ export async function createUser(email, password) {
 
 // Function to sigin user with email and password
 export async function signinUser(email, password) {
-  firebaseApp
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      // Signed in
-      const user = userCredential.user;
-      console.log('Created User with uid = ', user.uid);
-      // ...
-    })
-    .catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(
-        'Error::Auth::Failed to create User',
-        errorCode,
-        errorMessage,
-      );
-      throw errorMessage;
-    });
+  try {
+    const userCredential = await firebaseApp
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+
+    console.log(userCredential);
+    const user = userCredential.user;
+    console.log('Created User with uid = ', user.uid);
+    return user;
+  } catch (error) {
+    /* handle error */
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(
+      'Error::Auth::Failed to create User',
+      errorCode,
+      errorMessage,
+    );
+    throw errorMessage;
+  }
 }
 
 export function setUserState(setUser) {
