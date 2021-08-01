407274fe4a1c89b71883dfbe9330d118e7dfa08d
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-01 13:20:25 +0530

    Fixed signup with email

--- src/shared/Authentication.js
+++ src/shared/Authentication.js
@@ -2,23 +2,24 @@ import {firebaseApp} from './FirebaseInit';
 
 // Function to create user with email and password
 export async function createUser(email, password) {
-  firebaseApp
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      const user = userCredential.user;
-      console.log('Created User with uid = ', user.uid);
-    })
-    .catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(
-        'Error::Auth::Failed to create User',
-        errorCode,
-        errorMessage,
-      );
-      throw errorMessage;
-    });
+  try {
+    const userCredential = await firebaseApp
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+
+    const user = userCredential.user;
+    console.log('Created User with uid = ', user.uid);
+    return user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(
+      'Error::Auth::Failed to create User',
+      errorCode,
+      errorMessage,
+    );
+    throw errorMessage;
+  }
 }
 
 // Function to sigin user with email and password
