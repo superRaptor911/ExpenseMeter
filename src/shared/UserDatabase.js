ff34aebdb9c32a7a348b978cb498cb6c9cf4f611
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-09 12:30:14 +0530

    Post signup 2

--- src/shared/UserDatabase.js
+++ src/shared/UserDatabase.js
@@ -16,3 +16,15 @@ export async function setupUser(userId, username) {
     console.error('UserDatabase::', e);
   }
 }
+
+export async function getUserInfo(userId) {
+  const data = await firestore.doc(`root/${userId}`).get();
+  if (!data.exists) {
+    console.error('UserDatabase::User data does not exists');
+    throw 'No user data';
+  }
+
+  return {username: data.data().username};
+}
+
+export async function addExpense(userId, data) {}
